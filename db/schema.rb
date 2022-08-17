269a1e335ed5e6332c3eb4ff65a9496aa46d36b8
Illarion Kopeika <larrypearson@yandex.ru>
2022-08-17 14:32:14 +0800

    Add photo column to venues table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_083504) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_063101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_083504) do
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   add_foreign_key "checkins", "users"
