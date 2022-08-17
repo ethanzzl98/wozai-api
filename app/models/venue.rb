5199dd23a3d43f8cfea80fa5d55ddbbc07bdf3de
Ethan Teh <tehzl@hotmail.com>
2022-08-17 18:02:04 +0800

    fix seed file

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,6 @@
 class Venue < ApplicationRecord
   has_many :photos
   has_many :categories, through: :venue_categories
-  has_many :checkins
+  has_many :checkins, dependent: :destroy
   has_many :users, through: :checkins
 end
