130c3b9c480b4f7c3be6ad99d12aaddf378dea1a
Illarion Kopeika <larrypearson@yandex.ru>
2022-08-23 14:59:53 +0800

    Add active storage

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,6 @@
 class Venue < ApplicationRecord
-  has_many :photos
+  # has_many :photos
+  has_one_attached :photo
   has_many :venue_categories, dependent: :destroy
   has_many :categories, through: :venue_categories
   has_many :checkins, dependent: :destroy
